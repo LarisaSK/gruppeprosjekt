d9464c51659ac35a5165741ec1c17c75233547dc
Larisa Saitova <sklarisa@hotmail.com>
2022-09-22 10:42:06 +0200

    Lagde noen endringer til vuiewmodel

---
+++ bacit-dotnet.MVC/Models/Employee/EmplyeeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace bacit_dotnet.MVC.Models.Employee
+{
+    public class EmplyeeViewModel
+    {
+        [Required (ErrorMessage ="Employee ID is required")]
+        [Display (Name ="Ansatt ID")]
+        [MinLength(7)]
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "First Name ID is required")]
+        [Display(Name = "Navn")]
+        public string EmpName { get; set; }
+      
+        [Required(ErrorMessage = "Team ID is required")]
+        [Display(Name ="Lag ID")]
+        public int TeamId { get; set; }
+        
+
+
+    }
+}
